5072f5c5452ee4c9573a844c1d1f20257d7fa225
Niklas Seppälä <nikke40@gmail.com>
2019-03-04 15:00:58 +0200

    Created script to layout.html that creates header for layout.html. Added jquery to layout.html. Created folders for styles and scripts.

---
+++ static/scripts/mainHeader.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#mainHeader")[0].innerHTML =
+        '<span style="color: #cc7a00">Welcome</span>'
+        + '<span style="color:#ffff4d;">.</span>'
+        + '<span style="color: #999999">to</span>'
+        + '<span style="color:#d3d9b4;">(</span>'
+        + '</span><span style="color: #cc7a00">My</span>'
+        + '<span style="color:#ffff4d;">.</span>'
+        + '<span style="color: #999999">domain</span>'
+        + '<span style="color:#d3d9b4;">())</span>'
+        + '<span style="color:#ffff4d;">;</span>'
+});

735156eb282ba5248dd2761998a1b22268dfa19b
Niklas Seppälä <nikke40@gmail.com>
2019-03-04 11:32:17 +0200

    Added login page skeleton and linked it with index page

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index_page():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
 
 
 if __name__ == '__main__':

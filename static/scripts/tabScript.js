1302d640f3a1106d247f03b44447334139d917c9
Niklas <nikke40@gmail.com>
2019-03-20 08:34:12 +0200

    files

--- static/scripts/tabScript.js
+++ static/scripts/tabScript.js
@@ -196,7 +196,75 @@ $(function () {
             charsBefore += lines[i].length;
         }
         result.charsBefore = charsBefore;
-
         return result;
     }
 })
+
+/**
+ * Class for stack-object instances.
+ * Implemented in a way that items can't be accessed all
+ * at once. Only allows acces to elements in LIFO princible.
+ * 
+ * Stack with following methods:
+ *    isEmpty()
+ *    push()
+ *    peek()
+ *    pop()
+ */
+class Stack {
+
+    constructor() {
+        var items = [];
+        /**
+         * Returns false if stack is empty.
+         * Otherwise returns true.
+         */
+        this.isEmpty = function () {
+
+            if (items.length === 0)
+                return true;
+            return false;
+        }
+
+        /**
+         * Pushes new item on top of the stack.
+         */
+        this.push = function (item) {
+            items.push(item);
+        }
+
+        /**
+
+         * Peeks on top of the stack.
+         * Doesn't remove item on top of the stack.
+         */
+        this.peek = function () {
+            if (this.isEmpty())
+                return "Empty Stack";
+            return items[items.length - 1];
+        }
+
+        /**
+         * Returns the item on top of the stack
+         * and removes it from stack.
+         */
+        this.pop = function () {
+            if (this.isEmpty())
+                return "Empty Stack";
+            return items.pop();
+        }
+    }
+}
+
+/**
+ * Class for line-object instaces.
+ */
+class Line {
+
+    constructor(content, lineNum) {
+        this.charsBefore = null;
+        this.content = content;
+        this.length = content.length;
+        this.lineNum = lineNum;
+    }
+}
